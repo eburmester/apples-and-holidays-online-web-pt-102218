c6eecd499bead4a5ed738c9a608e9b336891f43c
Elyse Burmester <burmesterelyse@gmail.com>
2018-11-07 00:41:09 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -65,7 +65,7 @@ def all_holidays_with_bbq(holiday_hash)
   holiday_hash.map do |season, holidays|
     holidays.map do |holiday, supplies|
       if supplies == "BBQ"
-       holiday 
+        holiday 
       end
     end
   end 
