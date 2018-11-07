a3135d4ef55e974838fae1e2d333beb2746529f1
Elyse Burmester <burmesterelyse@gmail.com>
2018-11-07 00:17:07 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -61,8 +61,8 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
   holiday_hash.each do |holiday, supplies|
-    puts holiday 
-    puts supplies
+    holiday 
+    supplies
   end
 end
 
